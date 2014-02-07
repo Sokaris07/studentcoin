8eb07f2a33418c79f9726e5299cdc00c238ad890
Miguel Monteiro <sokaris91@gmail.com>
2014-02-07 18:19:07 +0000

    First bit changed

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 0, // Studentcoin addresses start with S
+        PUBKEY_ADDRESS = 63, // Studentcoin addresses start with S
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
